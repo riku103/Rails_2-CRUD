dae628e3a9f21dc616f3dae763a0d4364387b1b7
新田陸人 <>
2020-12-24 07:42:41 +0900

    Add: 編集完了画面へのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
   get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 end
