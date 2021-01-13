94e590f31098bbbd725cc8f7fbed1c0d1706100c
新田陸人 <>
2021-01-13 18:22:09 +0900

    詳細表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
